9e348f960fd5d8fbbb12a62ce1fb252574d7c771
Y_Y <85477034+Edmund-Y@users.noreply.github.com>
2023-05-31 09:56:31 +0900

    메인 페이지 및 기본JS 업데이트
    관광정보지도 추가 및 타일그리드 기능 추가

--- keys/vworld.js
+++ keys/vworld.js
@@ -19,3 +19,10 @@ let Base = new ol.layer.Tile({
         url: "https://api.vworld.kr/req/wmts/1.0.0/CEB52025-E065-364C-9DBA-44880E3B02B8/Base/{z}/{y}/{x}.png"
     })
 });
+
+let tileGrid = new ol.layer.Tile({
+    source: new ol.source.TileDebug({
+        projection: 'EPSG:3857',
+        tileGrid: Map.getAllLayers()[0].getSource().getTileGrid()
+    })
+})
