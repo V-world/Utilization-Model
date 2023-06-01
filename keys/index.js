5e437eb2ba807f8c33481779d74b5ec5383cf91b
Y_Y <85477034+Edmund-Y@users.noreply.github.com>
2023-06-01 14:45:23 +0900

    활용모델 모음페이지 개선

--- keys/index.js
+++ keys/index.js
@@ -6,9 +6,17 @@ function deatil(model){
     let modelName = model.querySelector("span").innerText;
     document.querySelector("#modelName").innerText = "□ "+modelName;
     document.querySelector("#modelLink").onclick = function () {
-        window.open(link);
+        window.open(link, "_blank");
+    }
+    document.querySelector("#imgTitle").onclick = function () {
+        window.open(link, "_blank");
     }
     document.querySelector("#imgs img").src = "./keys/imgs/" + link.split("/")[2] + ".png";
+    document.querySelector("#subtitle").innerText = model.querySelector("vp").innerText;
+    document.querySelector("#help").innerText = model.querySelector("vt").innerText;
+    document.querySelector("#githubDownload").onclick = function () {
+        window.open("https://github.com/V-world/Utilization-Model/tree/main" + link.replace(".", "").replace("/index.html", ""), "_blank");
+    }
 }
 
 
